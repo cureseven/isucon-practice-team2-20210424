0e3773ee1fd281d5a990c3badc1b8f7fdf04b391
Yu0614 <azu369yu@gmail.com>
2021-04-24 16:39:10 +0900

    feat: login のsqlを直してみた

--- ruby/app.rb
+++ ruby/app.rb
@@ -138,7 +138,7 @@ class App < Sinatra::Base
 
   post '/login' do
     name = params[:name]
-    statement = db.prepare('SELECT * FROM user WHERE name = ?')
+    statement = db.prepare('SELECT id, password, name FROM user WHERE name = ?')
     row = statement.execute(name).first
     if row.nil? || row['password'] != Digest::SHA1.hexdigest(row['salt'] + params[:password])
       return 403
